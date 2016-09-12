df0bbb34278ffc2287910d91f4659f462f82075f
q9002285117 <105021038@live.asia.edu.tw>
2016-09-12 15:47:24 +0800

    hw00-1

---
+++ hw00-1/src/apple.java
@@ -0,0 +1,15 @@
+
+public class apple {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("³ÅÚ{¶v");
+		System.out.println("105021038");
+		System.out.println("csie");
+	}
+
+}
